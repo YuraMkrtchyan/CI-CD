00881eb4bcae79d531c8148b8f0039a75d55bab8
yuramkrtchyan <yura.mkrtchyan01@gmail.com>
2023-05-29 14:52:02 +0400

    health check

---
+++ src/main/java/com/example/cicd/controller/HealthCheck.java
@@ -0,0 +1,16 @@
+package com.example.cicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HealthCheck {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "application is running";
+    }
+
+}
